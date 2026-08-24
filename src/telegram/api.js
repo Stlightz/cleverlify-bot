5feb9a865c865c86411e91c2d298f0d43e96fab5
Mikhail <mishakusq@gmail.com>
2026-08-24 16:44:47 +0300

    Update api.js

--- src/telegram/api.js
+++ src/telegram/api.js
@@ -1,1 +1,19 @@
-
+export async function sendTelegramMessage(chatId, text, env) {
+  const responce = await fetch(
+    `https://api.telegram.org/bot${env.CLEVERLIFY_TOKEN}/sendMessage`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text,
+      }),
+    }
+    );
+  if(!responce.ok) {
+    throw new Error(`Telegram API error: ${responce.status}`);
+  }
+  return responce.json();
+}
