7a170fca9c8e98650e6ec511126d4e10870ed888
Mikhail <mishakusq@gmail.com>
2026-08-24 16:48:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ export default {
   async fetch(request, env, ctx) {
     return new Response("AI Telegram Bot is alive!");
   }
+
+ import {sendTelegramMessage} from "./telegram/api.js";
+ await sendTelegramMessage(chatId, text, env);
 };
